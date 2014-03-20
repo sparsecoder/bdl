1516b260c294bf77161ed7fea1e72cd51488961a
andrew <andrew.stevens@pnnl.gov>
2014-03-20 13:37:01 -0400

    cleanup

--- plotDicts.m
+++ plotDicts.m
@@ -1,21 +1,25 @@
 function plotDicts(Dcell, filename)
 N = length(Dcell);
 W = ceil(sqrt(N));
-REM = W^2 - N > W;
-H = REM*(W-1)  +  ~REM*W;
+H = height(W,N);
 
 fig = figure('visible', 'off');
 for i=1:N
     n = size(Dcell{i},2);
     w = ceil(sqrt(n));
-    rem = w^2 - n;
-    h = (rem > w)*(w-1)  +  (rem <= w)*w;
+    [h, dif] = height(w,n);
     
     p = size(Dcell{i},1);
     
     subaxis(W,H,i, 'SpacingVert', 0.04,'SpacingHoriz', 0, 'Padding', 0, 'MarginTop', 0.027, 'MarginBottom', 0, 'MarginRight', 0, 'MarginLeft', 0);
-    d = displayPatches([Dcell{i} zeros(p,rem)]);
+    d = displayPatches([Dcell{i} zeros(p,dif)]);
     imshow(d(1:h*(sqrt(p)+1) + 1, :), 'Border', 'tight');
-    title(i, 'FontSize', 6); set(gca,'xtick',[],'ytick',[]);
+    title(i, 'FontSize', 6);
+    set(gca,'xtick',[],'ytick',[]);
 end
+
 saveas(fig, filename, 'pdf')
+
+function [h, dif] = height(w,n)
+dif = w^2 - n;
+h = (dif > w)*(w-1)  +  (dif <= w)*w;
