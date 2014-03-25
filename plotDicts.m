4bc5040c15e42916af170b2b839ceeeb899f86ad
andrew <andrew.stevens@pnnl.gov>
2014-03-25 15:33:44 -0400

    changed plot dicts to plot images

--- plotDicts.m
+++ plotDicts.m
@@ -2,20 +2,24 @@ function plotDicts(Dcell, filename)
 N = length(Dcell);
 W = ceil(sqrt(N));
 H = height(W,N);
-
+W=2;
+H=8;
 fig = figure('visible', 'off');
 for i=1:N
-    n = size(Dcell{i},2);
-    w = ceil(sqrt(n));
-    [h, dif] = height(w,n);
+%     n = size(Dcell{i},2);
+%     w = ceil(sqrt(n));
+%     [h, dif] = height(w,n);
+%     
+%     p = size(Dcell{i},1);
     
-    p = size(Dcell{i},1);
+     subaxis(W,H,i, 'SpacingVert', 0.04,'SpacingHoriz', 0, 'Padding', 0, 'MarginTop', 0.027, 'MarginBottom', 0, 'MarginRight', 0, 'MarginLeft', 0);
+%     d = displayPatches([Dcell{i} zeros(p,dif)]);
+%     imshow(d(1:h*(sqrt(p)+1) + 1, :), 'Border', 'tight');
     
-    subaxis(W,H,i, 'SpacingVert', 0.04,'SpacingHoriz', 0, 'Padding', 0, 'MarginTop', 0.027, 'MarginBottom', 0, 'MarginRight', 0, 'MarginLeft', 0);
-    d = displayPatches([Dcell{i} zeros(p,dif)]);
-    imshow(d(1:h*(sqrt(p)+1) + 1, :), 'Border', 'tight');
+    imagesc(Dcell{i});
     title(i, 'FontSize', 6);
     set(gca,'xtick',[],'ytick',[]);
+    colormap('gray')
 end
 
 saveas(fig, filename, 'pdf')
